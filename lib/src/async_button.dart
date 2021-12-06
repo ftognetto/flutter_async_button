50d3df391e50c79505ed6b6eff80a1a36a331fbe
Fabrizio Tognetto <info@quantos.it>
2021-12-06 16:42:21 +0100

    changes

--- lib/src/async_button.dart
+++ lib/src/async_button.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 enum AsyncButtonType {
-  ICON,
-  ELEVATED,
-  TEXT,
-  OUTLINE
+  icon,
+  elevated,
+  text,
+  textIcon,
+  outlined
 }
 
 class AsyncButton extends StatefulWidget {
@@ -12,17 +13,15 @@ class AsyncButton extends StatefulWidget {
   /// Can be IconButton, RaisedButton, FlatButton or OutlinedButton
   final AsyncButtonType type;
 
-  /// In case of IconButton
-  final Widget? icon;
 
-  /// In case of IconButton. If null [CenterLoading] is rendered
-  final Widget? busyIcon;
 
-  /// The text of the button
-  final Text? text;
+  // /// In case of IconButton. If null [CenterLoading] is rendered
+  // final Widget? busyIcon;
 
-  /// The color of the button
-  final Color color;
+  /// The child of the button, usually [Text] for [ElevatedButton] [TextButton] or [OutlinedButton] and [Icon] for [IconButton]
+  final Widget? child;
+
+  final Widget Function(AsyncStatus)? childBuilder;
 
   /// The color of the loading circle while button is busy
   final Color? loadingColor;
@@ -30,116 +29,139 @@ class AsyncButton extends StatefulWidget {
   /// The color of the background while button is busy
   final Color? loadingBackgroundColor;
 
-  /// The border radius of the button
-  final BorderRadius? borderRadius;
+  final ButtonStyle? style;
 
-  /// The elevation of the elevated button
-  final double? elevation;
+  /// Only for [TextIconButton]
+  final Widget? label;
 
-  final AsyncStatus Function()? busyBuilder;
 
   final Future<void> Function()? onPressed;
 
-  const AsyncButton.icon({ required this.icon, this.busyIcon, this.onPressed, this.color = Colors.grey, this.loadingColor, this.loadingBackgroundColor = Colors.grey, this.busyBuilder, Key? key}) : 
-    type = AsyncButtonType.ICON,
-    text = null,
-    borderRadius = null,
-    elevation = 0,
-    assert(icon != null),
+  const AsyncButton.icon({ this.child, this.style, this.onPressed, this.loadingColor, this.loadingBackgroundColor, this.childBuilder, Key? key}) : 
+    assert(child != null || childBuilder != null),
+    type = AsyncButtonType.icon,
+    label = null,
     super(key: key);
 
-  const AsyncButton.elevatedButton({required this.text, this.onPressed, this.color = Colors.grey, this.borderRadius = BorderRadius.zero, this.loadingColor, this.loadingBackgroundColor = Colors.grey, this.elevation, this.busyBuilder, Key? key}):
-    type = AsyncButtonType.ELEVATED,
-    icon = null,
-    busyIcon = null,
-    assert(text != null),
+  const AsyncButton.elevatedButton({ this.child, this.style, this.onPressed, this.loadingColor, this.loadingBackgroundColor,  this.childBuilder, Key? key}):
+    assert(child != null || childBuilder != null),
+    type = AsyncButtonType.elevated,
+    label = null,
     super(key: key);
 
-  const AsyncButton.outlineButton({required this.text, this.onPressed, this.color = Colors.grey, this.borderRadius = BorderRadius.zero, this.loadingColor, this.loadingBackgroundColor = Colors.grey, this.busyBuilder, Key? key}):
-    type = AsyncButtonType.OUTLINE,
-    icon = null,
-    busyIcon = null,
-    elevation = 0,
-    assert(text != null),
+  const AsyncButton.outlineButton({ this.child, this.style, this.onPressed, this.loadingColor, this.loadingBackgroundColor, this.childBuilder, Key? key}):
+    assert(child != null || childBuilder != null),
+    type = AsyncButtonType.outlined,
+    label = null,
     super(key: key);
   
-  const AsyncButton.textButton({required this.text, this.onPressed, this.color = Colors.grey, this.loadingColor, this.loadingBackgroundColor = Colors.grey, this.busyBuilder, Key? key}):
-    type = AsyncButtonType.TEXT,
-    icon = null,
-    busyIcon = null,
-    borderRadius = null,
-    elevation = 0,
-    assert(text != null),
+  const AsyncButton.textButton({ this.child, this.style, this.onPressed, this.loadingColor, this.loadingBackgroundColor, this.childBuilder, Key? key}):
+    assert(child != null || childBuilder != null),
+    type = AsyncButtonType.text,
+    label = null,
+    super(key: key);
+
+  const AsyncButton.textIconButton({required this.label, this.child, this.style, this.onPressed, this.loadingColor, this.loadingBackgroundColor, this.childBuilder, Key? key}):
+    assert(child != null || childBuilder != null),
+    type = AsyncButtonType.textIcon,
     super(key: key);
 
   @override
   _AsyncButtonState createState() => _AsyncButtonState();
 }
 
-enum AsyncStatus { IDLE, BUSY, SUCCESS, FAILURE }
+enum AsyncStatus { idle, busy, success, failure }
 
 class _AsyncButtonState extends State<AsyncButton> {
 
-  AsyncStatus _status = AsyncStatus.IDLE;
+  AsyncStatus _status = AsyncStatus.idle;
 
   @override
   Widget build(BuildContext context) {
 
-    final status = (widget.busyBuilder != null) ? widget.busyBuilder!() : _status;
 
     switch (widget.type) {
-      case AsyncButtonType.ICON:
+      case AsyncButtonType.icon:
         return IconButton(
-          icon: _buttonChild(status),
+          icon: widget.childBuilder != null ? widget.childBuilder!(_status) : _iconButtonChild(_status),
           onPressed: _onPressed
         );
-      case AsyncButtonType.ELEVATED:
+      case AsyncButtonType.elevated:
+        var style = widget.style ?? ElevatedButton.styleFrom();
+        if (_status != AsyncStatus.idle) { style = style.copyWith(backgroundColor: MaterialStateProperty.all<Color>(widget.loadingBackgroundColor ?? Theme.of(context).primaryColor)); }
         return ElevatedButton(
-          child: _buttonChild(status),
-          style: ElevatedButton.styleFrom(
-            shape: widget.borderRadius != null ? RoundedRectangleBorder(borderRadius: widget.borderRadius!) : RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-            primary: status == AsyncStatus.IDLE ? widget.color : widget.loadingBackgroundColor,
-            elevation: widget.elevation ?? 2
-          ),
+          child: widget.childBuilder != null ? widget.childBuilder!(_status) : _elevatedButtonChild(_status),
+          style: style,
           onPressed: _onPressed
         );
-      case AsyncButtonType.TEXT:
+      case AsyncButtonType.text:
         return TextButton(
-          child: _buttonChild(status),
-          style: TextButton.styleFrom(textStyle: TextStyle(color: widget.color)),
+          child: widget.childBuilder != null ? widget.childBuilder!(_status) : _textButtonChild(_status),
+          style: widget.style ?? TextButton.styleFrom(),
           onPressed: _onPressed
         );
-      case AsyncButtonType.OUTLINE:
+      case AsyncButtonType.textIcon:
+        return TextButton.icon(
+          label: widget.label!,
+          icon: widget.childBuilder != null ? widget.childBuilder!(_status) : _iconButtonChild(_status),
+          style: widget.style ?? TextButton.styleFrom(),
+          onPressed: _onPressed
+        );
+      case AsyncButtonType.outlined:
         return OutlinedButton(
-          child: _buttonChild(status),
-          style: OutlinedButton.styleFrom(
-            //backgroundColor: widget.color,
-            textStyle: TextStyle(color: widget.color)
-          ),
+          child: widget.childBuilder != null ? widget.childBuilder!(_status) : _outlinedButtonChild(_status),
+          style: widget.style ?? OutlinedButton.styleFrom(),
           onPressed: _onPressed
         );
     }
   }
 
-  Widget _buttonChild(AsyncStatus status) {
+  Widget _outlinedButtonChild(AsyncStatus status) {
     switch (status) {
-      case AsyncStatus.BUSY: return _Loading(color: widget.loadingColor ?? widget.color,);
-      case AsyncStatus.SUCCESS: return _Success(color: widget.loadingColor ?? widget.color);
-      case AsyncStatus.FAILURE: return _Failure(color: widget.loadingColor ?? widget.color);
-      case AsyncStatus.IDLE: return widget.type == AsyncButtonType.ICON ? widget.icon! : widget.text!;
+      case AsyncStatus.busy: return _Loading(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.success: return _Success(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.failure: return _Failure(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.idle: return widget.child!;
+    }
+  }
+
+  Widget _iconButtonChild(AsyncStatus status) {
+    switch (status) {
+      case AsyncStatus.busy: return _Loading(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.success: return _Success(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.failure: return _Failure(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.idle: return widget.child!;
+    }
+  }
+
+  Widget _elevatedButtonChild(AsyncStatus status) {
+    switch (status) {
+      case AsyncStatus.busy: return _Loading(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.success: return _Success(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.failure: return _Failure(color: widget.loadingColor ?? Theme.of(context).colorScheme.secondary);
+      case AsyncStatus.idle: return widget.child!;
+    }
+  }
+
+  Widget _textButtonChild(AsyncStatus status) {
+    switch (status) {
+      case AsyncStatus.busy: return _Loading(color: widget.loadingColor ?? Theme.of(context).colorScheme.primary);
+      case AsyncStatus.success: return _Success(color: widget.loadingColor ?? Theme.of(context).colorScheme.primary);
+      case AsyncStatus.failure: return _Failure(color: widget.loadingColor ?? Theme.of(context).colorScheme.primary);
+      case AsyncStatus.idle: return widget.child!;
     }
   }
 
   void _onPressed() {
     if (widget.onPressed == null) return;
-    if (_status != AsyncStatus.IDLE) return;
-    setState(() { _status = AsyncStatus.BUSY; });
+    if (_status != AsyncStatus.idle) return;
+    setState(() { _status = AsyncStatus.busy; });
     widget.onPressed!().then((_){
-      if (mounted) { setState((){ _status = AsyncStatus.SUCCESS; }); }
+      if (mounted) { setState((){ _status = AsyncStatus.success; }); }
     }).catchError((_){
-      if (mounted) { setState(() { _status = AsyncStatus.FAILURE; }); }
+      if (mounted) { setState(() { _status = AsyncStatus.failure; }); }
     }).whenComplete(() {
-      Future.delayed(Duration(seconds: 2), () { if (mounted) { setState((){ _status = AsyncStatus.IDLE; }); } }) ;
+      Future.delayed(const Duration(seconds: 2), () { if (mounted) { setState((){ _status = AsyncStatus.idle; }); } }) ;
     }); 
   }
 }
@@ -147,23 +169,20 @@ class _AsyncButtonState extends State<AsyncButton> {
 
 class _Loading extends StatelessWidget {
 
-  final Color? color;
+  final Color color;
 
-  const _Loading({Key? key, this.color }) : super(key: key);
+  const _Loading({Key? key, required this.color }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _color = color ?? Theme.of(context).accentColor;
     Widget loading = SizedBox(
-      width: 33,
-      height: 33,
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: CircularProgressIndicator(
-            strokeWidth: 1.5,
-            valueColor: AlwaysStoppedAnimation<Color>(_color),
-          )
+      width: 22,
+      height: 22,
+      child: CircularProgressIndicator(
+        strokeWidth: 1.5,
+        valueColor: AlwaysStoppedAnimation<Color>(color),
       )
+      
     );
     loading = Center(
       child: loading
@@ -180,7 +199,7 @@ class _Success extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _color = color ?? Theme.of(context).accentColor;
+    final _color = color ?? Theme.of(context).colorScheme.secondary;
     return Icon(Icons.check, color: _color);
   }
 }
@@ -193,7 +212,7 @@ class _Failure extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _color = color ?? Theme.of(context).accentColor;
+    final _color = color ?? Theme.of(context).colorScheme.secondary;
     return Icon(Icons.close, color: _color);
   }
 }
